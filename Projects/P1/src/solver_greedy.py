9a7910d955fe7db23bba1a6b5a3ac04d83b3a6b1
kiko-g <kikojpgoncalves@gmail.com>
2020-03-16 01:46:48 +0000

    Tweaks

--- Projects/P1/src/solver_greedy.py
+++ Projects/P1/src/solver_greedy.py
@@ -1,7 +1,47 @@
 import sys
-
 from src.files import parse_input, dump_rides
 from src.objects.Car import Car
+
+global_score = 0
+# 1st and 2nd files are almost instant
+# 3rd file takes 00:04:30
+# 4th file takes 00:04:46
+# 5th file takes 00:04:35
+# all combined take 00:13:51
+
+
+def special_print(n):
+    number = str(n)
+    result = ""
+    for i in range(len(number), 0, -1):
+        if ((i + 1) % 4) == 0:
+            result += ","
+        result += number[i - 1]
+
+    return result[::-1]
+
+
+def run(filename):
+    global global_score
+    file = "../assets/input/" + filename
+    rides, rows, cols, n_vehicles, bonus, t = parse_input(file + ".in")
+    cars = [Car(i + 1) for i in range(n_vehicles)]
+
+    while len(rides) > 0:
+        chosen_car = min(cars, key=lambda car_in_cars: car_in_cars.current_t)
+        chosen_ride = max(rides, key=lambda ride_in_rides: score_ride(chosen_car, ride_in_rides, bonus))
+        chosen_car.add_ride(chosen_ride, bonus)
+        rides.remove(chosen_ride)
+
+    dump_rides(file + ".out", cars)
+
+    score = 0
+    for car in cars:
+        for ride in car.rides:
+            score += ride.score
+
+    global_score += score
+    print("Score for file {} -->\t\t{}".format(filename, score))
 
 
 def score_ride(car_to_score, ride_to_score, bonus_to_score):
@@ -14,24 +54,15 @@ def score_ride(car_to_score, ride_to_score, bonus_to_score):
 
 
 if __name__ == '__main__':
-    file = "../assets/input/" + sys.argv[1]
-    rides, rows, cols, n_vehicles, bonus, t = parse_input(file + ".in")
-    cars = [Car(i + 1) for i in range(n_vehicles)]
+    if len(sys.argv) > 1:
+        run(sys.argv[1])
 
-    while len(rides) > 0:
-        chosen_car = min(cars, key=lambda car_in_cars: car_in_cars.current_t)
-        chosen_ride = max(rides, key=lambda ride_in_rides: score_ride(chosen_car, ride_in_rides, bonus))
+    else:
+        run("a_example")
+        run("b_should_be_easy")
+        run("c_no_hurry")
+        run("d_metropolis")
+        run("e_high_bonus")
 
-        chosen_car.add_ride(chosen_ride, bonus)
-
-        rides.remove(chosen_ride)
-
-    dump_rides(file + ".out", cars)
-
-    score = 0
-    for car in cars:
-        for ride in car.rides:
-            score += ride.score
-
-    print("Score for file {} -->\t{}".format(sys.argv[1], score))
+        print("\nGlobal score is {}".format(special_print(global_score)))
 
